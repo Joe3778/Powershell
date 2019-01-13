54fe99c41cd2bf0d927fe939a43a510f60d91088
Joe3778 <33321995+Joe3778@users.noreply.github.com>
2019-01-13 12:48:04 -0600

    v0.3

--- ReplSync.ps1
+++ ReplSync.ps1
@@ -1,58 +1,32 @@
-#Created by Joe Bechler
-#01-11-2019
-#This script can either be run manually or set as a scheduled task to send alert email and resume sync.
+##Created by Joe Bechler
+##01-11-2019
+##This script can either be run manually or set as a scheduled task to send alert email and resume sync.
 
 
-#Run command and store output
+##Run command and store output
 ##The real check
 $cmdOutput = symrdf list -all | Out-String
 ##Testing check
-#$cmdOutput = type "C:\Users\Joe\Documents\GitHub\Powershell\Sample.out" | Out-String
+#$cmdOutput = type "C:\Scripts\Sample.out" | Out-String
 
 $cmdOutputSplit = $cmdOutput -split "`n"
 
-#$SuspendedLine = ($cmdOutput | Select-String -Pattern 'Suspended').Matches.Value
-
-#$SuspendedLines = $cmdOutputSplit | Select-String -Pattern 'Suspended' -AllMatches | %{$_.Line}
-#$SuspendedLines = $cmdOutputSplit | Select-String -Pattern 'Suspended' -AllMatches 
 $SuspendedLines = $cmdOutputSplit | Select-String -Pattern 'Suspended' -AllMatches | Out-String
-#| %{$_.Line}
+#write-host $SuspendedLines
 
 if (!$SuspendedLines) {exit}
 
-
-#write-host $SuspendedLines
-
 $SuspendedGroups = ($SuspendedLines | Select-String -Pattern 'R1:\d{1,}' -AllMatches).Matches.Value | Out-String
-
 #write-host $SuspendedGroups
-
-#$SuspendedUniqueGroup = Select-Object -InputObject $SuspendedGroups -Unique | Out-String
-
-#$contents = $SuspendedGroups
-#$hash = @{}
-
-#foreach($content in $contents)
-#{
-#    $temp = $content.split(',')
-#    if($hash.ContainsKey($temp[0]))  
-#    {
-#        $hash.Remove($temp[0])
- #   }
-#    $hash.add($temp[0],$temp[1]);
-#}
-
-#$hash
 
 $SuspendedGroups = $SuspendedGroups -split "`n"
 
-write-host "All Groups found:"
-$SuspendedGroups
+#write-host "All Groups found:"
+#$SuspendedGroups
 
-#$lines = $SuspendedGroups
 $TEST = @{}
 
-# Remove Duplicates
+## Remove Duplicates
 $UniqueGroups = Foreach ($line in $SuspendedGroups)
 {
  if ($TEST.$line)
@@ -68,36 +42,36 @@ $UniqueGroups = Foreach ($line in $SuspendedGroups)
 }
 
 
-write-host "Duplicate Groups Rremoved:"
-$UniqueGroups
+#write-host "Duplicate Groups Rremoved:"
+#$UniqueGroups
 
 
-# Process EACH LINE
+## Process EACH LINE
 Foreach ($Group in $UniqueGroups)
 {
  $UniqueGroupNum = ($Group | Select-String -Pattern ':\d{1,}').Matches.Value
  $UniqueGroupNum = $UniqueGroupNum -replace '[:]',''
  if ($UniqueGroupNum) {
-	#$command = 'symrdf resume -file rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt'
-	#$command = 'dir *.*$UniqueGroupNum'
-	#Invoke-Expression $command
-	#Invoke-Expression 'symrdf resume -file rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt'
-	#$WhatDone += 'symrdf resume -file rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt' | out-string
-	#Invoke-Expression 'dir *.*$UniqueGroupNum'
-	$WhatDone += "symrdf resume -file rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt" | out-string
-	#$WhatDone += $command | out-string
+	#Invoke-Expression 'symrdf resume -file D:\VMAX_CLI_Files\rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt'
+	#Invoke-Expression 'symrdf $UniqueGroupNum' ##For Testing
+	$WhatDone += "symrdf resume -file D:\VMAX_CLI_Files\rdfg$UniqueGroupNum.txt -sid 696 -rdfg $UniqueGroupNum -noprompt`n"
+	$WhatDone += "`r`n"
 	}
  
 }
-$WhatDone
+#write-host "Commands Run:"
+#$WhatDone
 
 
 ##Add some text to the body and include the command output
 $body =@"
-Here is the sync report for s1vipems01 using 'symrdf list -all'
+Here are the suspended groups:
+$SuspendedLines
+
+Commands run to resume them:
 $WhatDone
+
 "@
 
-Send-MailMessage command to send the email.
-Send-MailMessage -From s1vipems01@coop.org -To "Joe Bechler <joe.bechler@coop.org>" -Subject "SYMRDF Results" -Body $body -Smtpserver TMGSMTP.tmg.net
-#, "Brad Lones <Brad.Lones@coop.org>", "Jonathan Lee <Jonathan.Lee@coop.org>"
+##Send email message
+Send-MailMessage -From s1vipems01@coop.org -To "Joe Bechler <joe.bechler@coop.org>", "Brad Lones <Brad.Lones@coop.org>", "Jonathan Lee <Jonathan.Lee@coop.org>" -Subject "VMAX Suspended Groups" -Body $body -Smtpserver TMGSMTP.tmg.net
